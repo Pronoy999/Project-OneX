b85b3bca9fc96c500ee7e7acd4f839ed5182b151
Writwick <writzx@gmail.com>
2017-08-26 08:11:29 +0530

    Incomplete commit. xD

---
+++ One X/One X/MPU.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace One_X {
+    public static class MPU {
+        public static byte A {
+            get {
+                return regA;
+            }
+            set {
+                regA = value;
+            }
+        }
+
+        private static byte regA, regB, regC, regD, regE, regH, regL;
+
+        static void loadB(byte x) {
+            regB = x;
+        }
+    }
+}
---
+++ One X/One X/Memory.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace One_X {
+    static class Memory {
+        
+    }
+}
