8919f3955beacef8e968e6fd38fd2b596d77ebaa
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2017-08-31 21:17:15 +0530

    Memory updated.

--- One X/One X/Memory.cs
+++ One X/One X/Memory.cs
@@ -6,7 +6,44 @@ using System.IO;
 using System.Threading.Tasks;
 
 namespace One_X {
-    static class Memory {
-        
+    static class Memory
+    {
+        static private FileStream fileStream;
+        static private String fileName = "Memory.dat";
+        static private short stackTop, stackTail;
+        static byte ReadByte(short offset)
+        {
+            fileStream=new FileStream(fileName,FileMode.Append);
+            fileStream.Seek(offset, SeekOrigin.Begin);
+            BinaryReader binaryReader=new BinaryReader(fileStream);
+            return binaryReader.ReadByte();
+        }
+
+        static void WriteByte(byte data,short offset)
+        {
+            fileStream = new FileStream(fileName, FileMode.Append);
+            fileStream.Seek(offset,SeekOrigin.Begin);
+            BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+            binaryWriter.Write(data);
+        }
+
+        static void PushStack(byte data)
+        {
+            fileStream = new FileStream(fileName, FileMode.Append);
+            fileStream.Seek(stackTop, SeekOrigin.Begin);
+            BinaryWriter binaryWriter = new BinaryWriter(fileStream);
+            binaryWriter.Write(data);
+        }
+
+        static byte PopStack()
+        {
+            byte data=0;
+            return data;  //TODO: Check the MPU stack Condition and then return data. 
+        }
+
+        static void InitStack()
+        {
+            //TODO: Initialize the Stack Top and Stack Tail. 
+        }
     }
 }
