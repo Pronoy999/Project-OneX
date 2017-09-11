d5f631e6281b86373671ca92d6aae5ac5216b4a9
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2017-09-11 20:12:44 +0530

    Parser class completed.

--- One X/One X/Parser.cs
+++ One X/One X/Parser.cs
@@ -1,33 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace One_X
 {
-    class Parser
-    {
-        #region Parser
-        public static void parse(String code, out List<Instruction> instructions)
-        {
-            instructions = new List<Instruction>();
-            char[] separator = { '\n' };
+    class Parser {
+        internal int startingAddress; // This is the staring address of the code. 
+        internal List<Instruction> instructions = new List<Instruction>();  // The list containing the instructins without the label.
+        internal Dictionary<int, String> labels = new Dictionary<int, string>();// The dictionary with the key as the memory address and the label as the value. 
+        public Parser(int startingAddress) {
+            this.startingAddress = startingAddress;
+        }
+        /**<summary>
+         * This is the method to call using the object of the parser class. 
+         * Create the object of the class and set the starting address. 
+         * </summary>
+         */
+        public void parse(String code) {
+            int address = startingAddress;
+            char[] newLine = { '\n' };
             char[] lineSeparator = { ':' };
-            String[] lines = code.Split(separator);
-            foreach(String line in lines){
-                if (line.Contains(":"))
-                {
-                    String[] instruct = line.Split(lineSeparator);
-                    instructions.Add(Instruction.parse(instruct[1].Trim()));
-                    //TODO: store the label.
+            String[] lines = code.Split(newLine);
+            foreach (String line in lines) {
+                if (line.Contains(":")) {
+                    String[] labelInst = line.Split(lineSeparator);
+                    Instruction inst = Instruction.parse(labelInst[1].Trim());
+                    instructions.Add(inst);
+                    labels.Add(address, labelInst[0].Trim());
+                    address += inst.Bytes;
                 }
-                else
-                {   //NO Label.
-                    instructions.Add(Instruction.parse(line.Trim()));
+                else {
+                    Instruction inst = Instruction.parse(line.Trim());
+                    instructions.Add(inst);
+                    address += inst.Bytes;
                 }
             }
         }
-        #endregion
     }
 }
