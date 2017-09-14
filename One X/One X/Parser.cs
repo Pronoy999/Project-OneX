491674e72ff9c1b17b7cbba6925ede88e43325f5
Pronoy Mukherjee <mukherjee_pronoy@yahoo.in>
2017-09-14 19:06:34 +0530

    Parser Updated to Handle Error.

--- One X/One X/Parser.cs
+++ One X/One X/Parser.cs
@@ -4,16 +4,21 @@ using System.Text.RegularExpressions;
 
 namespace One_X
 {
+    /**<summary>
+     * This is the Enum for the different parts of the Instructions. 
+     * </summary>
+     */
     class Parser {
         enum StringType {
             Label,
-            Mneumonic,
-            Literal
+            Mnemonic,
+            Literal,
+            Error
         }        
         internal int startingAddress; // This is the staring address of the code. 
         internal List<Instruction> instructions = new List<Instruction>();  // The list containing the instructins without the label.
         internal Dictionary<int, String> labels = new Dictionary<int, string>();// The dictionary with the key as the memory address and the label as the value.        
-        List<Tuple<StringType, int, int, int,String>> instructionList = new List<Tuple<StringType,int, int,  int,String>>();
+        List<Tuple<StringType, int, int, int>> instructionList = new List<Tuple<StringType,int, int,  int>>();//StringType,lineIndex,ColIndex,
         string regex14 = "^[0-9a-fA-F]{1,4}H?$";
         string regbex12 = "^[0-9a-fA-F]{1,2}H?$";
         public Parser(int startingAddress) {
@@ -27,46 +32,76 @@ namespace One_X
         public void parse(String code) {
             StringType type;
             int lineInd=0;
-            int colInd;
             int length;           
             int address = startingAddress;
             char[] newLine = { '\n' }; 
             char[] lineSeparator = { ':' };
-            String[] lines = code.Split(newLine);
-            foreach (String line in lines) {               
-                if (line.Contains(":")) {
-                    String[] labelInst = line.Split(lineSeparator);
-                    Instruction inst = Instruction.parse(labelInst[1].Trim());
-                    instructions.Add(inst);
-                    labels.Add(address, labelInst[0].Trim());
-                    instructionList.Add(Tuple.Create<StringType, int, int, int,String>(StringType.Label, lineInd, 0, labelInst[0].Length,labelInst[0]));
-                    if (inst.Bytes >= 2)   // For 2 or 3 Byte Instructions Length = length+1;
-                        length = inst.Name.Length + 1;
-                    else length = inst.Name.Length;
-                    if (inst.Bytes == 2) {
-                        Regex reg = new Regex(regbex12, RegexOptions.Singleline);
-                        try {
-                            String lit = labelInst[1].Substring(length).Trim();
-                            Match match = reg.Match(lit);
-                            if (match.Success) {
-                                instructionList.Add(Tuple.Create<StringType, int, int, int,String>(StringType.Mneumonic, lineInd, labelInst[0].Length + 1, length,labelInst[1].Substring(labelInst[0].Length,length+1)));
-                                //TODO: Add the Literal Part. 
+            Boolean hasTwoColons = code.IndexOf(lineSeparator[0]) == code.LastIndexOf(lineSeparator[0]);
+            if (!hasTwoColons) {
+                String[] lines = code.Split(newLine);
+                foreach (String line in lines) {
+                    if (line.Contains(":")) {
+                        String[] labelInst = line.Split(lineSeparator);
+                        Instruction inst = Instruction.parse(labelInst[1].Trim());
+                        if (!(inst.Name.Equals(String.Empty))) {
+                            instructions.Add(inst);
+                            labels.Add(address, labelInst[0].Trim());
+                            address += inst.Bytes; //Increasing the Address Local Variable. 
+                            instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Label, lineInd, 0, labelInst[0].Length));// Inserting the label. 
+                            if (inst.Bytes >= 2)   // For 2 or 3 Byte Instructions Length = length+1;
+                                length = inst.Name.Length + 1;
+                            else length = inst.Name.Length;
+                            if (inst.Bytes == 2) {
+                                Regex reg = new Regex(regbex12, RegexOptions.Singleline);
+                                try {
+                                    String lit = labelInst[1].Substring(length).Trim();
+                                    Match match = reg.Match(lit);
+                                    instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Mnemonic, lineInd, labelInst[0].Length + 2, length));//Adding the Mneumonics
+                                    if (match.Success) {                                        
+                                        instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Literal, lineInd, length, lit.Length));//Adding the Literal
+                                    }
+                                    else {
+                                        instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Error, -1, -1, -1));
+                                        //TODO: Throw Exception.
+                                    }
+                                }
+                                catch (Exception e) {
+                                    //TODO:Catch the Exception.
+                                }
                             }
-                            else {
-                                //Throw NoMatchException
+                            else if (inst.Bytes==3){
+                                Regex reg = new Regex(regex14, RegexOptions.Singleline);
+                                try {
+                                    String lit=labelInst[1].Substring(length).Trim();
+                                    Match match = reg.Match(lit);
+                                    instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Mnemonic, lineInd, labelInst[0].Length + 2, length));//Adding the Mneumonics
+                                    if (match.Success) {                                        
+                                        instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Literal, lineInd, length, lit.Length));//Adding the Literal.
+                                    }
+                                    else {
+                                        instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Error, -1, -1, -1));
+                                    }
+                                }
+                                catch (Exception e) {
+                                    //Handle the Regex No match Exception.
+                                }
                             }
                         }
-                        catch(Exception e) {
-                            //TODO:Catch the Exception.
+                        else {
+                            instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Error, -1, -1, -1)); //ERROR for NoSuchInstruction. 
                         }
                     }
+                    else {
+                        Instruction inst = Instruction.parse(line.Trim());
+                        instructions.Add(inst);
+                        address += inst.Bytes;
+                    }
+                 lineInd++;
+                    
                 }
-                else {
-                    Instruction inst = Instruction.parse(line.Trim());
-                    instructions.Add(inst);
-                    address += inst.Bytes;
-                }
-                lineInd++;
+            }
+            else {
+                instructionList.Add(Tuple.Create<StringType, int, int, int>(StringType.Error, -1, -1, -1));  // Putting the Error with more than One Colons. 
             }
         }
     }
