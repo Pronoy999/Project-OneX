85bab190cbe9a83f48860604f3f613983cd33a62
Writwick Das <w4r1k3d@outlook.com>
2017-10-06 22:00:13 +0530

    updated with goto offset

--- One X/One X/MemoryViewer.Designer.cs
+++ One X/One X/MemoryViewer.Designer.cs
@@ -24,6 +24,11 @@
         /// </summary>
         private void InitializeComponent() {
             this.memBox = new Be.Windows.Forms.HexBox();
+            this.statusPanel = new System.Windows.Forms.Panel();
+            this.offsetLabel = new System.Windows.Forms.Label();
+            this.offset = new System.Windows.Forms.TextBox();
+            this.gotoBtn = new System.Windows.Forms.Button();
+            this.statusPanel.SuspendLayout();
             this.SuspendLayout();
             // 
             // memBox
@@ -37,21 +42,69 @@
             this.memBox.Name = "memBox";
             this.memBox.ShadowSelectionColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(60)))), ((int)(((byte)(188)))), ((int)(((byte)(255)))));
             this.memBox.ShadowSelectionVisible = false;
-            this.memBox.Size = new System.Drawing.Size(600, 729);
+            this.memBox.Size = new System.Drawing.Size(600, 697);
             this.memBox.TabIndex = 0;
             this.memBox.UseFixedBytesPerLine = true;
             this.memBox.VScrollBarVisible = true;
+            this.memBox.SelectionStartChanged += new System.EventHandler(this.memBox_SelectionStartChanged);
+            // 
+            // statusPanel
+            // 
+            this.statusPanel.Controls.Add(this.gotoBtn);
+            this.statusPanel.Controls.Add(this.offset);
+            this.statusPanel.Controls.Add(this.offsetLabel);
+            this.statusPanel.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.statusPanel.Location = new System.Drawing.Point(0, 697);
+            this.statusPanel.Name = "statusPanel";
+            this.statusPanel.Size = new System.Drawing.Size(600, 32);
+            this.statusPanel.TabIndex = 1;
+            // 
+            // offsetLabel
+            // 
+            this.offsetLabel.AutoSize = true;
+            this.offsetLabel.Location = new System.Drawing.Point(431, 6);
+            this.offsetLabel.Name = "offsetLabel";
+            this.offsetLabel.Size = new System.Drawing.Size(60, 22);
+            this.offsetLabel.TabIndex = 0;
+            this.offsetLabel.Text = "Offset:";
+            this.offsetLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // offset
+            // 
+            this.offset.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.offset.Font = new System.Drawing.Font("Hack", 12F);
+            this.offset.Location = new System.Drawing.Point(488, 3);
+            this.offset.Name = "offset";
+            this.offset.Size = new System.Drawing.Size(46, 26);
+            this.offset.TabIndex = 1;
+            this.offset.Text = "0000";
+            this.offset.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.offset.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.offset_KeyPress);
+            // 
+            // gotoBtn
+            // 
+            this.gotoBtn.Font = new System.Drawing.Font("Trebuchet MS", 10F);
+            this.gotoBtn.Location = new System.Drawing.Point(537, 3);
+            this.gotoBtn.Name = "gotoBtn";
+            this.gotoBtn.Size = new System.Drawing.Size(60, 26);
+            this.gotoBtn.TabIndex = 2;
+            this.gotoBtn.Text = "GOTO";
+            this.gotoBtn.UseVisualStyleBackColor = true;
+            this.gotoBtn.Click += new System.EventHandler(this.gotoBtn_Click);
             // 
             // MemoryViewer
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
             this.ClientSize = new System.Drawing.Size(600, 729);
             this.Controls.Add(this.memBox);
+            this.Controls.Add(this.statusPanel);
+            this.Font = new System.Drawing.Font("Trebuchet MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "MemoryViewer";
             this.Text = "MemoryViewer";
             this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MemoryViewer_FormClosing);
             this.Load += new System.EventHandler(this.MemoryViewer_Load);
+            this.statusPanel.ResumeLayout(false);
+            this.statusPanel.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -59,5 +112,9 @@
         #endregion
 
         public Be.Windows.Forms.HexBox memBox;
+        private System.Windows.Forms.Panel statusPanel;
+        private System.Windows.Forms.Label offsetLabel;
+        private System.Windows.Forms.Button gotoBtn;
+        private System.Windows.Forms.TextBox offset;
     }
 }
--- One X/One X/MemoryViewer.cs
+++ One X/One X/MemoryViewer.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -15,14 +16,49 @@ namespace One_X {
             InitializeComponent();
         }
 
+        [DllImport("user32.dll")]
+        static extern bool HideCaret(IntPtr hWnd);
+
         private void MemoryViewer_Load(object sender, EventArgs e) {
             memBox.Font = Fonts.Fonts.Create(Fonts.FontFamily.Hack, 12);
             memBox.ByteProvider = MPU.memory.provider;
+
+            offset.Font = Fonts.Fonts.Create(Fonts.FontFamily.Hack, 12);
+
+            offset.GotFocus += (sndr, args) => {
+                offset.Select(offset.TextLength, 0);
+                HideCaret(offset.Handle);
+            };
         }
 
         private void MemoryViewer_FormClosing(object sender, FormClosingEventArgs e) {
             e.Cancel = true;
             Hide();
+        }
+
+        private void offset_KeyPress(object sender, KeyPressEventArgs e) {
+            char c = e.KeyChar;
+            if (!((c >= '0' && c <= '9') || (c >= 'A' && c <= 'F') || (c >= 'a' && c <= 'f'))) {
+                e.Handled = true;
+                if (c == (char)Keys.Return) {
+                    gotoBtn.PerformClick();
+                }
+                return;
+            }
+            var x = sender as TextBox;
+            if (x.TextLength == 4) {
+                x.Text = x.Text.Substring(1);
+                x.SelectionStart = x.TextLength;
+            }
+        }
+
+        private void memBox_SelectionStartChanged(object sender, EventArgs e) {
+            offset.Text = memBox.SelectionStart.ToString("X4");
+        }
+
+        private void gotoBtn_Click(object sender, EventArgs e) {
+            memBox.Select(long.Parse(offset.Text, System.Globalization.NumberStyles.HexNumber), 0);
+            memBox.Focus();
         }
     }
 }
